6e047a3eebc0eeac1215dc563e2ce94187a2b97c
Waterfox <waterfox80@gmail.com>
2023-09-26 09:43:10 +0200

    Delete slider script

--- javascript/script.js
+++ javascript/script.js
@@ -50,7 +50,7 @@ function getCity(cp)
 }
 
 
-function getWeather(insee)
+function getWeather(insee, day)
 {
     //console.log(insee);
     fetch(`https://api.meteo-concept.com/api/forecast/daily?token=196a097c4b2b523569d2099ae7fd051b2afbacf5b97bf204b9876facfe68d66e&insee=${insee}`)
@@ -58,19 +58,9 @@ function getWeather(insee)
     .then(
         data =>
         {
-            for(let i = 1; i < 8; i++){
-                let divDay = document.getElementById(`day${i}`);
-                let weather = data.forecast[i-1];
-                divDay.innerText = "Temp min : " + weather.tmin + "\n"; 
-                divDay.innerText += "Temp max : " + weather.tmax + "\n"; 
-                divDay.innerText += "Probabilité pluie : " + weather.probarain + "\n"; 
-                divDay.innerText += "Nombre heure ensoleillement : " + weather.sun_hours + "\n"; 
-                divDay.innerText += "Latitude : " + weather.latitude + "\n"; 
-                divDay.innerText += "Longitude : " + weather.longitude + "\n"; 
-                divDay.innerText += "Cumul de la pluie sur la journée : " + weather.rr10 + "mm\n"; 
-                divDay.innerText += "Vitesse du vent à 10m : " + weather.wind10m + "km/h\n"; 
-                divDay.innerText += "Direction du vent à 10m : " + weather.dirwind10m + "°\n";
-                divDay.innerText += "Temps : " + weather.weather + "\n";
+            for(let i = 1; i < 8; i++)
+            {
+                
             }
             document.getElementById("arrow").style.transform = "rotate(" + weather.dirwind10m + "deg)";
             //console.log(document.getElementById("arrow").style.transform);
@@ -80,18 +70,3 @@ function getWeather(insee)
 }
 
 let strWeatherInfos = ["°C", "°C", "%", "H", "", "", "mm", "km/h", "°", ""];
-
-for(let i = 1; i < 11; i++){
-    document.getElementById("weatherInfos-Text" + i).innerText = document.getElementById("tempSlider" + i).value + strWeatherInfos[i - 1];
-    document.getElementById("tempSlider" + i).addEventListener('input', () =>
-    {
-        document.getElementById("weatherInfos-Text" + i).innerText = document.getElementById("tempSlider" + i).value + strWeatherInfos[i - 1];
-        if(i == 9){
-            document.getElementById("arrow").style.transform = "rotate(" + document.getElementById("tempSlider" + i).value + "deg)";
-            //console.log(document.getElementById("arrow").style.transform)
-        }
-        if(i = 10){
-            weatherIcon(document.getElementById("tempSlider" + i).value);
-        }
-    })
-}
