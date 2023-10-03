8e7dd4bb3f7005c9616ae752b049d282ecb4ca8b
Waterfox <waterfox80@gmail.com>
2023-10-03 09:57:43 +0200

    Add SVG

--- javascript/scriptWeather.js
+++
@@ -1,8 +0,0 @@
-function weatherIcon(value){
-    let img = document.getElementById("weatherIcon");
-    switch(parseInt(value)){
-        case 1 : 
-            img.src = "assets/weather/1.svg";
-            console.log("Yes : " + value + " " + img.src);
-    }
-}
